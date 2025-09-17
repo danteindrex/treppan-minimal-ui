a9cda546e4d90ce6ab32760feac7cfc78bf6e44a
Dan Trevor Matovu <trevorsdanny@gmail.com>
2025-09-17 21:50:24 +0300

    loveable

--- eslint.config.js
+++ eslint.config.js
@@ -8,10 +8,7 @@ export default tseslint.config(
   { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    ,
     },
     plugins: {
       "react-hooks": reactHooks,
@@ -19,8 +16,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "@typescript-eslint/no-unused-vars": "off",
+    
     },
   },
 );
